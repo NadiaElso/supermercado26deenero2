3fdd4d4eec5b32c3c8f2db7df0417a457e635554
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-26 19:41:50 -0300

    supermercado26deenero3

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ let precioManteca: number = 80;
 let precioQueso: number = 120;
 let precioFideos: number = 70;
 let preciosalsaDeTomates: number = 80;
-let precioAceite:number=90;
+let precioAceite: number = 90;
 let precioCremaDeLeche: number = 90;
 let precioDulceDeLeche: number = 60;
 let precioGalletitas: number = 40;
@@ -61,12 +61,11 @@ console.log("El precion unitario es:" + precioCremaDeLeche);
 console.log("El precion unitario es:" + precioDulceDeLeche);
 
 //let ingresonuevoproducto = prompt("Ingresa el nombre del nuevo producto");
-let stockinicialnuevoproducto=(
-  //prompt("Ingresa el stock inicial del nuevo producto")
+let stockinicialnuevoproducto = prompt(
+  "Ingresa el stock inicial del nuevo producto"
 );
-let ingresoprecionuevoproducto = (
- 
-  //prompt("Ingresa el  precio del nuevo producto")
+let ingresoprecionuevoproducto = prompt(
+  "Ingresa el  precio del nuevo producto"
 );
 let productonuevo = ingresonuevoproducto;
 let stocknuevoproducto = stockinicialnuevoproducto;
@@ -75,33 +74,44 @@ console.log(
   `Nuevo producto es, ${productonuevo}, su stock inicial es, ${stocknuevoproducto}, su precio inicial es, ${precionuevoproducto} `
 );
 
-
 console.log("Bienvenido a supermercado Madariaga");
 console.log("Los productos disponibles son:");
 
-if (stockLeche!=0) {
-  console.log("Leche $"+ precioLeche);
-} if (stockAzucar!=0) {
-  console.log("Azucar $" +precioAzucar);
-}if (stockYogurt!=0) {
+if (stockLeche != 0) {
+  console.log("Leche $" + precioLeche);
+}
+if (stockAzucar != 0) {
+  console.log("Azucar $" + precioAzucar);
+}
+if (stockYogurt != 0) {
   console.log("Yogurt $" + precioYogurt);
-}if (stockManteca!=0) {
+}
+if (stockManteca != 0) {
   console.log("Manteca $" + precioManteca);
-}if (stockQueso!=0) {
+}
+if (stockQueso != 0) {
   console.log("Queso $" + precioQueso);
-}if (stockFideos!=0) {
-  console.log("Fideo $"  + precioFideos);
-
-}if (stockSalsaDeTomate!=0) {
+}
+if (stockFideos != 0) {
+  console.log("Fideo $" + precioFideos);
+}
+if (stockSalsaDeTomate != 0) {
   console.log("Salsa de Tomate $" + preciosalsaDeTomates);
-}if (StockAceite!=0) {
+}
+if (StockAceite != 0) {
   console.log("Aceite $" + precioAceite);
-}if (StockCremaDeLeche!=0) {
+}
+if (StockCremaDeLeche != 0) {
   console.log("Crema de Leche $" + precioCremaDeLeche);
-}if (stockDulceDeLeche!=0) {
+}
+if (stockDulceDeLeche != 0) {
   console.log("Dulce de Leche $" + precioDulceDeLeche);
-}if (stockGalletitas!=0) {
+}
+if (stockGalletitas != 0) {
   console.log("Galletitas $" + precioGalletitas);
-}if (stocknuevoproducto!=0) {
+}
+if (stocknuevoproducto != 0) {
   console.log(productonuevo + precionuevoproducto);
 }
+
+let compradeproducto: number = Number("producto a comprar");
